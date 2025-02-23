1bb0c68f3fa1788311ac612a5a404bbbacf013b7
joker <joker03081991@gmail.com>
2025-02-23 15:24:35 +0900

    add update_driver file

---
+++ update_driver.py
@@ -0,0 +1,15 @@
+import os
+import config
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+chrome_driver_path = config.CHROMEDRIVER_PATH
+
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+
+
+driver.get("https://www.google.com")
+print("ChromeDriver is updated and working!")
+driver.quit()
