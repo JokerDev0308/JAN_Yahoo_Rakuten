dec2d63f87760c8fdbe100319310e3bff18f6921
joker <joker03081991@gmail.com>
2025-02-25 02:02:58 +0900

    modified config driver path

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 HEADLESS = True  # Set to False if you want to see browser running
 TIMEOUT = 10  # Selenium wait timeout
-CHROMEDRIVER_PATH = "drivers/chromedriver-linux64"
+CHROMEDRIVER_PATH = "drivers/chromedriver-linux64/chromedriver"
 JANCODE_SCV = 'data/jan_codes.csv'
 OUTPUT_XLSX = 'data/scraped_prices.xlsx'
 
