8eac279e485f557913926ccfbc7ad41dce59c121
joker <joker03081991@gmail.com>
2025-02-26 17:53:29 +0900

    modified app

--- app.py
+++ app.py
@@ -17,10 +17,25 @@ class PriceScraperUI:
         uploaded_file = st.file_uploader("Choose a CSV file with JAN codes", type="csv")
         if uploaded_file is not None:
             jan_df = pd.read_csv(uploaded_file)
+            
             st.write("JAN Codes loaded:", len(jan_df))
-            st.dataframe(jan_df)
+            jan_df.index = jan_df.index + 1
+            height = min(len(jan_df) * 35 + 38, 800)
+            
+            st.dataframe(jan_df, use_container_width=True, height=height, key = "jancode_update")
+
             jan_df.to_csv(JANCODE_SCV, index=False)
             st.success(f"JAN codes saved to {JANCODE_SCV}")
+
+        else:
+            try:
+                df = pd.read_excel(JANCODE_SCV)
+                df.index = df.index + 1
+                height = min(len(df) * 35 + 38, 800)
+                st.dataframe(df, use_container_width=True, height=height, key = "jancode_original")
+                
+            except FileNotFoundError:
+                st.warning("No scraped data available yet.")
 
     def _setup_scraping_controls(self):
         st.write("### Scraping Control")
