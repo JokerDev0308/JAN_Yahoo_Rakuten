88c12fed335ac4ae211f64d88c6ae37b08ad7d15
joker <joker03081991@gmail.com>
2025-03-03 03:14:07 +0900

    update --rakuten

--- scripts/rakuten_scraper.py
+++ scripts/rakuten_scraper.py
@@ -27,6 +27,7 @@ class RakutenScraper:
             # Click the filter button if its value is not 0
             if filter_button.get_attribute('value') != '0':
                 filter_button.click()
+                print(filter_button)
                 sleep(2)
 
             # Wait for the final price elements to load
