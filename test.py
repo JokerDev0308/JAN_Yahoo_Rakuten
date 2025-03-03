b2e44911ad8ca4a6dbddc65c2155b53de90dacb8
joker <joker03081991@gmail.com>
2025-03-04 00:43:50 +0900

    test yahoo

--- test.py
+++ test.py
@@ -37,9 +37,11 @@ class YahooScraper:
         try:
             self.driver.get(f"https://shopping.yahoo.co.jp/search?p={jan_code}")
             
-            WebDriverWait(self.driver, TIMEOUT).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "SearchResult_SearchResult__cheapestButton__SFFlT"))
-            )
+            # WebDriverWait(self.driver, TIMEOUT).until(
+            #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".SearchResult_SearchResult__cheapestButton__SFFlT"))
+            # )
+
+            sleep(3)
 
             items = self.driver.find_elements(By.CSS_SELECTOR, ".LoopList__item")
 
@@ -50,7 +52,7 @@ class YahooScraper:
                 price = self._extract_price_from_item(item)
                 if price and price < min_price:
                     link = item.find_element(By.CSS_SELECTOR, 
-                        "SearchResult_SearchResult__cheapestButton__SFFlT")
+                        ".SearchResult_SearchResult__cheapestButton__SFFlT")
                     if link:
                         min_price = price
                         min_price_link = link
